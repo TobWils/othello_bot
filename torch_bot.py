dffebefd881b44b15a504dbf49280003cfc14c11
TobWils <toby@linsow.com>
2026-02-22 00:42:44 +0000

    torch bot should function now with alpha zero

--- torch_bot.py
+++ torch_bot.py
@@ -132,10 +132,11 @@ class ResBlock(nn.Module):
         x = F.relu(x)
         return x
 
-
 class ResNet(nn.Module):
-    def __init__(self, game: othello, num_res_blocks, num_hidden):
+    def __init__(self, game: othello, num_res_blocks, num_hidden, device):
         super().__init__()
+
+        self.device = device
 
         self.start_block = nn.Sequential(
             nn.Conv2d(3, num_hidden, kernel_size=3, padding=1),
@@ -163,6 +164,8 @@ class ResNet(nn.Module):
             nn.Linear(3*game.ACTION_SIZE, 1),
             nn.Tanh()
         )
+
+        self.to(device)
     
     def forward(self, x):
         x = self.start_block(x)
@@ -172,47 +175,23 @@ class ResNet(nn.Module):
         value = self.value_head(x)
         return policy, value
 
-def test_model():
-    Othello = othello()
-
-    board = Othello.create_board()
-
-    Othello.print_board(board)
-
-    encoded_board = Othello.get_encoded_board(board)
-
-    print(encoded_board)
-
-    tensor_board = torch.tensor(encoded_board).unsqueeze(0)
-
-    model = ResNet(Othello, 4, 64)
-
-    policy, value = model(tensor_board)
-    value = value.item()
-    policy = torch.softmax(policy, axis = 1).squeeze(0).detach().cpu().numpy()
-
-    print(value, policy)
-
-    plt.bar(range(Othello.ACTION_SIZE), policy)
-    plt.show()
-
 class Node():
-    def __init__(self, game: othello, args, board, player, parent=None, move=None):
+    def __init__(self, game: othello, args, board, player, parent=None, move=None, prior=0, number_visits=0):
         self.game = game
         self.args = args
         self.board = board
         self.player = player
         self.parent = parent
         self.move = move
+        self.prior = prior
 
         self.childeren = []
-        self.expandable_moves = game.valid_moves(board, player)
 
-        self.number_visits = 0
+        self.number_visits = number_visits
         self.total_val = 0
     
     def is_fully_expanded(self):
-        return np.sum(self.expandable_moves) == 0 and len(self.childeren) > 0
+        return len(self.childeren) > 0
     
     def select(self):
         best_child = None
@@ -227,51 +206,26 @@ class Node():
         return best_child
     
     def UCB(self, child):
-        q_value = 1 - (child.total_val/child.number_visits + 1)/2 # want to be a number in range 0 to 1 and to be low so that the win prob is minimised for oponent
-        return q_value + self.args["C"]*np.sqrt(np.log(self.number_visits)/child.number_visits)
+        if child.number_visits == 0:
+            q_value = 0
+        else:
+            q_value = 1 - (child.total_val/child.number_visits + 1)/2 # want to be a number in range 0 to 1 and to be low so that the win prob is minimised for oponent
+        return q_value + child.prior*self.args["C"]*np.sqrt(self.number_visits/(child.number_visits + 1))
     
-    def expand(self):
-        if np.sum(self.expandable_moves) != 0:
-            move = np.random.choice(np.where(self.expandable_moves == 1)[0])
-            self.expandable_moves[move] = 0
+    def expand(self, policy):
+        if np.sum(policy) != 0:
+            for move, prob in enumerate(policy):
+                if prob > 0:
+                    child_board = self.board.copy()
+                    child_board = self.game.change_perspective(child_board, player = -1) # switch perspectives so that freindly peices are always 1, this is done before moves as the available moves depends on the curent player
+                    child_board = self.game.make_move(child_board, 1, move)
+                    child_player = -self.player
 
-            child_board = self.board.copy()
-            child_board = self.game.change_perspective(child_board, player = -1) # switch perspectives so that freindly peices are always 1, this is done before moves as the available moves depends on the curent player
-            child_board = self.game.make_move(child_board, 1, move)
-            child_player = -self.player
-
-            child = Node(self.game, self.args, child_board, child_player, self, move)
-            self.childeren.append(child)
-
-            return child
+                    child = Node(self.game, self.args, child_board, child_player, self, move, prob)
+                    self.childeren.append(child)
         else:
             child = Node(self.game, self.args, self.board, -self.player, self)
             self.childeren.append(child)
-
-            return child
-    
-    def simulate(self):
-        value, is_terminal = self.game.get_value_and_terminated(self.board, self.player, self.move)
-        value = -value
-
-        if is_terminal:
-            return value
-        
-        rollout_board = self.board.copy()
-        rollout_board = self.game.change_perspective(rollout_board, player = -1)
-        rollout_player = 1 # in this implementation we only ever show the bot a board that has 1's for its peices and -1's for oponent peices hence we set the rolout player to be 1
-        while True:
-            valid_moves = self.game.valid_moves(rollout_board, rollout_player)
-            if np.sum(valid_moves) != 0:
-                move = np.random.choice(np.where(valid_moves == 1)[0])
-                rollout_board = self.game.make_move(rollout_board, rollout_player, move)
-                value, is_terminal = self.game.get_value_and_terminated(rollout_board, self.player, move)
-                if is_terminal:
-                    if rollout_player == -1:
-                        value = -value
-                    return value
-            
-            rollout_player = -rollout_player
     
     def backpropigate(self, value):
         self.total_val += value
@@ -282,12 +236,29 @@ class Node():
             self.parent.backpropigate(value)
 
 class MCTS():
-    def __init__(self, game: othello, args):
+    def __init__(self, game: othello, args, model: ResNet):
         self.game = game
         self.args = args
+        self.model = model
 
+    @torch.no_grad()
     def search(self, board, player):
-        root = Node(self.game, self.args, board, player)
+        root = Node(self.game, self.args, board, player, number_visits=1)
+
+        policy, _ = self.model(
+            torch.tensor(
+                self.game.get_encoded_board(board),
+                device=self.model.device
+            ).unsqueeze(0)
+        )
+        policy = torch.softmax(policy, axis=1).squeeze(0).cpu().numpy()
+        policy = (1 - self.args["dirichlet_epsilon"])*policy + self.args["dirichlet_epsilon"]*np.random.dirichlet([self.args["dirichlet_alpha"]] * self.game.ACTION_SIZE)
+        
+        valid_moves = self.game.valid_moves(board, player)
+        policy *= valid_moves
+        policy /= np.sum(policy)
+        root.expand(policy)
+
 
         for search in range(self.args["num_searches"]):
             node = root
@@ -299,8 +270,21 @@ class MCTS():
             value = -value # as the move stored in a node is the move made by the oponent so the value should be inverted
 
             if not is_terminal:
-                node = node.expand()
-                value = node.simulate()
+                policy, value = self.model(
+                    torch.tensor(
+                        self.game.get_encoded_board(node.board),
+                        device=self.model.device
+                    ).unsqueeze(0)
+                )
+                policy = torch.softmax(policy, axis=1).squeeze(0).cpu().numpy()
+                valid_moves = self.game.valid_moves(node.board,node.player)
+                policy *= valid_moves
+                if np.sum(policy) != 0:
+                    policy /= np.sum(policy)
+
+                value = value.item()
+
+                node.expand(policy)
             
             node.backpropigate(value)
 
@@ -311,17 +295,131 @@ class MCTS():
 
         return action_probs
 
+class AlphaZero():
+    def __init__(self, model: ResNet, optimiser: torch.optim, game: othello, args):
+        self.model = model
+        self.optimiser = optimiser
+        self.game = game
+        self.args = args
+        self.mcts = MCTS(game, args, model)
+    
+    def self_play(self):
+        memory = []
+        player = 1
+        board = self.game.create_board()
+
+        while True:
+            if np.sum(self.game.valid_moves(board, player)) !=0:
+                neutral_board = self.game.change_perspective(board, player)
+                action_probs = self.mcts.search(neutral_board, 1)
+
+                memory.append((neutral_board, action_probs, player))
+
+                temperature_action_probs = action_probs ** (1/self.args["temperature"])
+                temperature_action_probs /= np.sum(temperature_action_probs)
+                move = np.random.choice(self.game.ACTION_SIZE, p=temperature_action_probs)
+
+                board = self.game.make_move(board, player, move)
+
+                value, is_terminal = self.game.get_value_and_terminated(board, player, move)
+
+                if is_terminal:
+                    return_memory = []
+                    for hist_neutral_board, hist_action_probs, hist_player in memory:
+                        hist_outcome = value if hist_player == player else -value
+                        return_memory.append((
+                            torch.tensor(
+                                self.game.get_encoded_board(hist_neutral_board)
+                            ),
+                            hist_action_probs,
+                            hist_outcome
+                        ))
+                    
+                    return return_memory
+            
+            player = -player
+
+    def train(self, memory):
+        np.random.shuffle(memory)
+        for batchIdx in range(0, len(memory), self.args["batch_size"]):
+            sample = memory[batchIdx : min(len(memory)-1,batchIdx + self.args["batch_size"])]
+            if len(sample) != 0:
+                boards, policy_targets, value_targets = zip(*sample)
+
+                boards, policy_targets, value_targets = np.array(boards), np.array(policy_targets), np.array(value_targets).reshape(-1,1)
+
+                boards = torch.tensor(boards, dtype=torch.float32, device=self.model.device)
+                policy_targets = torch.tensor(policy_targets, dtype=torch.float32, device=self.model.device)
+                value_targets = torch.tensor(value_targets, dtype=torch.float32, device=self.model.device)
+
+                out_policy, out_value = self.model(boards)
+
+                policy_loss = F.cross_entropy(out_policy,policy_targets)
+                value_loss = F.mse_loss(out_value, value_targets)
+
+                loss = policy_loss + value_loss
+
+                self.optimiser.zero_grad()
+                loss.backward()
+                self.optimiser.step()
+
+    def learn(self):
+        for iteration in range(self.args["num_iterations"]):
+            memory = []
+
+            self.model.eval()
+            for self_play_iteration in range(self.args["num_self_play_iterations"]):
+                memory += self.self_play()
+            
+            self.model.train()
+            for epoch in range(self.args["num_epochs"]):
+                self.train(memory)
+            
+            torch.save(self.model.state_dict(), f"torch_bot_save_file/model_{iteration}.pt")
+            torch.save(self.optimiser.state_dict(), f"torch_bot_save_file/optimiser_{iteration}.pt")
+
+
+def test_model():
+    Othello = othello()
+
+    board = Othello.create_board()
+
+    Othello.print_board(board)
+
+    encoded_board = Othello.get_encoded_board(board)
+
+    print(encoded_board)
+
+    tensor_board = torch.tensor(encoded_board).unsqueeze(0)
+
+    device = torch.device(torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu")
+
+    model = ResNet(Othello, 4, 64, device)
+    model.load_state_dict(torch.load("torch_bot_save_file/model_2.pt"))
+    model.eval()
+
+    policy, value = model(tensor_board)
+    value = value.item()
+    policy = torch.softmax(policy, axis = 1).squeeze(0).detach().cpu().numpy()
+
+    print(value, policy)
+
+    plt.bar(range(Othello.ACTION_SIZE), policy)
+    plt.show()
 
 def main():
     game = othello()
     player = 1
 
     args = {
-        "C": 1.41,
+        "C": 2,
         "num_searches": 1000
     }
 
-    mcts = MCTS(game, args)
+    model = ResNet(game, 4, 64)
+    model.eval()
+
+    mcts = MCTS(game, args, model)
 
     board = game.create_board()
 
@@ -357,4 +455,28 @@ def main():
         
         player = -player
 
-test_model()
+def test_alphazero():
+    Othello = othello()
+
+    device = torch.device(torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu")
+
+    model = ResNet(Othello, 4, 64, device)
+
+    optimiser = torch.optim.Adam(model.parameters(), lr = 10**(-3), weight_decay=10**(-4))
+
+    args = {
+        "C": 2,
+        "num_searches": 60,
+        "num_iterations": 3,
+        "num_self_play_iterations": 10,
+        "num_epochs": 4,
+        "batch_size": 2,
+        "temperature": 1.25,
+        "dirichlet_epsilon": 0.25,
+        "dirichlet_alpha": 0.3
+    }
+
+    alphaZero = AlphaZero(model, optimiser, Othello, args)
+    alphaZero.learn()
+
+test_alphazero()
